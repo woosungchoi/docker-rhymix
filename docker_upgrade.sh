26c5fb7a3abf25d4ae0f710b9af07afde97e5661
WoosungChoi <solut2000@gmail.com>
2020-09-10 22:13:11 +0900

    Update docker_upgrade.sh

--- docker_upgrade.sh
+++ docker_upgrade.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
-cd /your/path/docker-multi-site/
+cd /your/path/to/docker-rhymix/
 
 $COMPOSE pull && $DOCKER pull php:7.4-fpm-alpine && $COMPOSE up --build -d && $DOCKER image prune -f
